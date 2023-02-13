43d03d20a53ba21eb75a4f65ee31e4ed6f682d23
baimuhtar <baimuhtar1997@gmail.com>
2023-02-13 17:05:40 +0600

    third task

--- src/Kyu8.java
+++ src/Kyu8.java
@@ -2,10 +2,11 @@ public class Kyu8 {
     public static void main(String[] args) {
         System.out.println(solution("world"));
         System.out.println(oppositeNumber(5));
+        System.out.println(dnaToRna("GCAT"));
     }
 
     /* Complete the solution so that it reverses the string passed into it.
-             'world'  =>  'dlrow'           'word'   =>  'drow'          */
+             'world' => 'dlrow'           'word' => 'drow'          */
     public static String solution(String str) {
         String reverseWord = "";
         for (int i = str.length() - 1; i >= 0; i--) {
@@ -18,6 +19,17 @@ public class Kyu8 {
             1: -1       14: -14        -34: 34                                   */
     public static int oppositeNumber(int number) {
         return number * -1;
+    }
+
+    /* Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems.
+      It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+      Ribonucleic acid, RNA, is the primary messenger molecule in cells.
+      RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+      Create a function which translates a given DNA string into RNA.
+      "GCAT"  =>  "GCAU" */
+
+    public static String dnaToRna(String dna) {
+        return dna.replace('U', 'T');
     }
 
 }
