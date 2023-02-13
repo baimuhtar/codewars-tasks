9786b7f109f0ef406b94f49576f70e08a8ce9820
baimuhtar <baimuhtar1997@gmail.com>
2023-02-13 16:55:46 +0600

    second task

--- src/Kyu8.java
+++ src/Kyu8.java
@@ -1,11 +1,11 @@
 public class Kyu8 {
     public static void main(String[] args) {
         System.out.println(solution("world"));
+        System.out.println(oppositeNumber(5));
     }
 
     /* Complete the solution so that it reverses the string passed into it.
-             'world'  =>  'dlrow'
-             'word'   =>  'drow'                                         */
+             'world'  =>  'dlrow'           'word'   =>  'drow'          */
     public static String solution(String str) {
         String reverseWord = "";
         for (int i = str.length() - 1; i >= 0; i--) {
@@ -13,4 +13,11 @@ public class Kyu8 {
         }
         return reverseWord;
     }
+
+    /* Very simple, given an integer or a floating-point number, find its opposite.
+            1: -1       14: -14        -34: 34                                   */
+    public static int oppositeNumber(int number) {
+        return number * -1;
+    }
+
 }
